5fb249636d83eda5c4f4e3b3fa57b6efd670dffd
Jesus Jose Moreno Pinto <jesusjm@zeta40.aulas.gsyc.es>
2016-03-15 13:21:50 +0100

    practica acabada

---
+++ worker.js
@@ -0,0 +1,19 @@
+
+postMessage("I'm working");
+
+onmessage = function (Event) {
+
+    var n = 1;
+    primelist = "";
+    search: while (n<Event.data) {
+      n += 1;
+      for (var i = 2; i <= Math.sqrt(n); i += 1)
+        if (n % i == 0)
+          continue search;
+      // found a prime!
+      primelist += " " + n;
+
+    }
+    postMessage(primelist);
+    //postMessage("Primos de "+Event.data);
+};
